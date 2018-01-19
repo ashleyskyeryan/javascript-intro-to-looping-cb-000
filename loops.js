b218b97295dd20ad5cbe4cac8e93438116b04b26
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 19:26:17 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-var forLoop = array => {
+function forLoop(array) {
   for (i = 0; i < 25; i++) {
     if (i === 1) array.push("I am 1 strange loop,");
     else array.push("I am ${i} strange loops.");
