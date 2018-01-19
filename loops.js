54b13759bba90b5cb4d7c33a14fb118321918061
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-19 19:20:34 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (i = 0; i < 25; i++) {
     if (i = 1) return "I am 1 strange loop,";
-    else (i = >=2 && <= 25)"I am ${i} strange loops.";
+    else (i = >=2 && <= 25) return"I am ${i} strange loops.";
   }
 }
